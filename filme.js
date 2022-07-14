93056a2a65446b3be96f9534456b98a90971cdb2
Wesley Oliveira Lima Da Silva <108889735+WesleyDev93@users.noreply.github.com>
2022-07-14 17:15:43 -0300

    Criando js

---
+++ filme.js
@@ -0,0 +1,32 @@
+function adicionarFilmeFavorito(){
+  var campofilmefavorito = document.querySelector('#filme')
+  //console.log (campofilmefavorito)
+  var filmeFavorito = campofilmefavorito.value 
+  if(filmeFavorito.endsWich('jpg'))
+  listarFilmesNaTela(filmeFavorito){
+    
+  }else{
+    alert("imagem invalida")
+  }
+   
+ 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ //document.write("<img src=" + filmeFavorito + ">")
+
+ //console.log("clicou")
+
